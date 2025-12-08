083cbdf4c5cb4b2c55a2d5d54a9a48353c582883
Fiachra Tobin <tobinfi@tcd.ie>
2025-12-08 13:03:44 +0000

    undid setup-database changes, unnecessary after all

--- setup-database.sh
+++ setup-database.sh
@@ -9,9 +9,6 @@ CREATE USER IF NOT EXISTS 'aichat_user'@'localhost' IDENTIFIED BY 'aichat_pass';
 GRANT ALL PRIVILEGES ON aichat_db.* TO 'aichat_user'@'localhost';
 FLUSH PRIVILEGES;
 
-USE aichat_db;
-ALTER TABLE chat_message MODIFY content LONGTEXT;
-
 SELECT 'Database setup complete!' AS '';
 EOF
 
